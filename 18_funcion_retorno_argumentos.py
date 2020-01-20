e0061f87a00f8f87c54d9e789507b31f3cb8f328
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-01-19 21:01:37 -0500

    Aplicaciones de las función.
    Definición de una función como asignación de argumentos de entrada y salida. Curso CEC. Python

---
+++ 18_funcion_retorno_argumentos.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Funciones con retorno de argumentos
+
+@author: David
+"""
+def multiplicar(a,b):    #definición de la función
+    return a*b           #retorno de la función
+
+def deseos():
+    print("\tfeliz cumpleanos") #cuando llamamos a la función, imprime un msm
+    return "happy birthday" #El valor de la función es "happy birthday"
