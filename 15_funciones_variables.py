bf44e7215331359b214f0441ccad0d06a92c4b07
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-01-19 21:04:18 -0500

    Funciones.
    Definición de una función con una y dos variables. Curso CEC Python.

---
+++ 15_funciones_variables.py
@@ -0,0 +1,23 @@
+"""
+
+Programa funciones
+
+Elaborado por:
+David Jara
+17 enero 2020
+
+"""
+
+"""
+    Definir una función con argumentos
+"""
+def datoNom(nom): #declara una función datoNom y una variable nom
+    print("Tu nombre es\n\t",nom) #Fin de la función
+
+#******************************************************
+
+def holaTodos(nom1,nom2):
+    print("Hola, tu nombre es\n\t",nom1)
+    print("Hola, es tu nombre ... \n\t",nom2)
+
+
