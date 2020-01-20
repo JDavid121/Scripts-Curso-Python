ceee5b6c1c83471709eee62418349fd0ec787b3c
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-01-19 21:21:30 -0500

    Aplicación de lazo for
    Programa que imprime todos los datos de una lista. Curso CEC Python.

---
+++ 06_for_function.py
@@ -0,0 +1,15 @@
+"""
+
+Evaluación del lazo for
+
+Realizado por: David Jara.
+Fecha: 17 enero de 2020
+"""
+
+devices=["r1","r2","r3","s1","s2"]  #Declaración de una lista de datos llamada devices
+print("***************************")
+print()
+print(devices)                      #Impresión de la lista devices
+print()
+for i in devices:                   #Lazo for que apunta a cada dato de la lista devices
+    print(i)                        #Imprime cada dato de la lista device
