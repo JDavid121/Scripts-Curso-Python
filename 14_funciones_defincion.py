a79fd7e10212141ba4d7abd63bbde55cb6f9dbc2
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-01-19 21:06:27 -0500

    Función.
    Definición de una función. Curso CEC Python.

---
+++ 14_funciones_defincion.py
@@ -0,0 +1,24 @@
+"""
+
+Programa definición de funciones
+
+Elaborado por:
+David Jara
+17 enero 2020
+
+"""
+
+"""
+    Definir una función que imprima un mensaje
+"""
+
+#Definición de la función
+def menInicio():                        #Con la palabra def se declara una función.
+    print("Inicio de la transmisión")   #La función debe tener una palabra () y los : puntos
+                                        #Fin de la función.
+                                        #Las funciones deben ser ideticas
+##Programa principal
+
+print("Transmisión de datos")
+menInicio()     #Llamada (invocación) de la función.
+print("Fin de la transmisión")
