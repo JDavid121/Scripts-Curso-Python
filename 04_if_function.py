fd359e36464106c35919923c3be0c7e2addf9883
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-01-19 20:38:20 -0500

    Descripción del comanado if
    Es una descripción operativa del comando if

---
+++ 04_if_function.py
@@ -0,0 +1,14 @@
+"""
+
+Evaluación de la función if
+
+Realizado por: David Jara.
+Fecha: 17 enero de 2020
+"""
+
+nativeVLAN=1        #Definición de la variable nativeVLAN
+dataVLAN=100        #Definición de la variable dataVLAN
+if nativeVLAN == dataVLAN:              #Condicional IF
+    print("The native VLAN and dataVLAN are the same")    #Sentencias ejecutadas si if == True
+else:
+    print("The native VLAN and data VLAN are not the same")    #Sentencias ejecutadas si if == False
