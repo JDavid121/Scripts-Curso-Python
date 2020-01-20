e1fe71ab515d7cfb5b44d6602bf4ee02acf71e4c
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-01-19 21:00:00 -0500

    Función de una lista.
    Programa que es una función de una lista. Aplicación del lazo for. Curso CEC Python

---
+++ 19_funcion_lista.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Función de una lista
+@author: David
+
+"""
+#namelist=["Andrés","Pablo", "Lucy","Camila"] poner esta lista en la consola
+def hieverybody(namelist):#función de una lista.
+    for name in namelist:#(a) los valores de la lista los almacena en name
+        print("hola",name) #Imprime el valor de la var name obtenida de (a)
+
