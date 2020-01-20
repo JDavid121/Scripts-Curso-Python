327c89c520907265f20602e095574bad3a7f8d6c
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-01-19 21:05:27 -0500

    Funciones.
    Definición de una función con tres argumentos y un argumento de salida. Curso CEC Python.

---
+++ 15_funciones_tres_var.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 19:30:29 2020
+
+@author: David
+"""
+
+#Creación de una función de tres variables
+def computo(a,b,c): #Defiición de una función de 3 variables
+    return a+b+c
+def direccion(calle,ciudad,codpostal):
+    print("Tu dirección es",calle,"tu ciudad es",ciudad,"codigo postal ",codpostal)
+    
