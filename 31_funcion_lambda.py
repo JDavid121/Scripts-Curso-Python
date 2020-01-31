fdf672d6faeaef94eaad49f2c6dc70ddd4fea877
JDavid121 <59768038+JDavid121@users.noreply.github.com>
2020-01-31 10:39:11 -0500

    Función Lambda
    Ejemplo de aplicación de la función Lambda. Curso Python CEC EPN.

---
+++ 31_funcion_lambda.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 20:42:47 2020
+
+@author: David
+"""
+
+"""
+A lambda function that sums argument a, b, and c and print the result:
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+"""
+x = lambda a, b, c : a + b + c   #Función lambda.
+                                # x = nombre de la función
+                                # lambda a, b, c : define los argumentos
+                                # : a + b +c define la salida de la función lambda
+# llamada de la función lambda: x(1,2,3)
+#Resultado esperado: Out[]: 6
